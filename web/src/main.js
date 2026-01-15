f6acf49fe00053029b203ccada3731b4c6598639
Vignesh Kumar Devaraj <vigneshkumar.devaraj@candascent.com>
2026-01-15 13:51:59 +0530

    modified choose file

--- web/src/main.js
+++ web/src/main.js
@@ -201,7 +201,11 @@ function init() {
           <div class="field">
             <label>Excel file</label>
             <div class="fileRow">
-              <input id="file" type="file" accept=".xlsx,.xls" />
+              <label for="file" class="fileBtn">
+                <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/><polyline points="17 8 12 3 7 8"/><line x1="12" y1="3" x2="12" y2="15"/></svg>
+                Choose File
+              </label>
+              <input id="file" type="file" accept=".xlsx,.xls" style="display: none;" />
               <div class="fileMeta">
                 <div class="fileName" id="fileName">No file chosen</div>
                 <div class="fileBadge" id="fileBadge">Upload an Excel file to begin</div>
